cce04fa9960112b5f658369874db92486fc6204f
Daniel Luis Costa <dluiscosta@gmail.com>
2020-12-28 17:51:23 -0300

    implemented _get_pascals_tiangle_row method

--- binomial_expansion/binomial_expander.py
+++ binomial_expansion/binomial_expander.py
@@ -6,6 +6,7 @@ class BinomialExpander:
     _EXPR_REGEX = re.compile(
         r"\((?P<a>\-?[0-9]*)(?P<x>[a-z])\+?(?P<b>\-?[0-9]+)\)\^(?P<n>[0-9]+)"
     )
+    _memorized_pascals_triangle_rows = [[1]]
 
     @classmethod
     def _extract_variables(cls, expr):
@@ -17,9 +18,15 @@ class BinomialExpander:
             int(match.group('b')), match.group('x'), int(match.group('n'))
         )
 
-    @staticmethod
-    def _get_pascals_triangle_row(n_choices):
-        raise NotImplementedError()
+    @classmethod
+    def _get_pascals_triangle_row(cls, n):
+        if n < len(cls._memorized_pascals_triangle_rows):
+            return cls._memorized_pascals_triangle_rows[n]
+        else:
+            previous_row = cls._get_pascals_triangle_row(n-1)
+            row = [a+b for a, b in zip([0]+previous_row, previous_row+[0])]
+            cls._memorized_pascals_triangle_rows.append(row)
+            return row
 
     @staticmethod
     def _build_polinomial(a, b, x, coeffs):
