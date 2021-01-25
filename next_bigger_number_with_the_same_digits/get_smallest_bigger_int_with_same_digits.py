294446d5b07ff67dc25a6f5d08b4e130bd3ffa21
Daniel Luis Costa <dluiscosta@gmail.com>
2021-01-24 22:35:09 -0300

    initial implementation

--- next_bigger_number_with_the_same_digits/get_smallest_bigger_int_with_same_digits.py
+++ next_bigger_number_with_the_same_digits/get_smallest_bigger_int_with_same_digits.py
@@ -8,7 +8,24 @@ Description:
 
 
 def get_smallest_bigger_int_with_same_digits(int_: int) -> int:
-    raise NotImplementedError
+    digits = [int(digit) for digit in str(int_)]
+    # look for a break in a monotonically increasing sequence, right to left
+    current_biggest_digit = 0
+    for backwards_idx, digit in enumerate(reversed(digits)):
+        if digit >= current_biggest_digit:  # sequence continues
+            current_biggest_digit = digit
+        else:  # found a break, one of the digits to be swapped
+            idx = len(digits) - backwards_idx - 1
+            # look for the leftmost smallest digit bigger than the break digit,
+            # inside the monotonically increasing sequence
+            replacement_idx, replacement = min(
+                [(i, d) for i, d in enumerate(digits[idx+1:], start=idx+1)
+                 if d > digit],
+                key=(lambda i_d: i_d[1]*len(digits) + i_d[0])
+            )
+            digits[idx], digits[replacement_idx] = replacement, digit  # swap
+            return int(''.join((str(d) for d in digits)))  # list->int convers.
+    return -1
 
 
 next_bigger = get_smallest_bigger_int_with_same_digits  # for Codewars
