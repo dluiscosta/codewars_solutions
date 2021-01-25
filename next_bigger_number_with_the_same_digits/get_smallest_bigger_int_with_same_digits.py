b8b9796805dabb19d6e3e7e1ec5ef33912fbc2ab
Daniel Luis Costa <dluiscosta@gmail.com>
2021-01-24 21:03:11 -0300

    factored get_smallest_bigger_int_with_same_digits and added tests

---
+++ next_bigger_number_with_the_same_digits/get_smallest_bigger_int_with_same_digits.py
@@ -0,0 +1,11 @@
+"""
+Solution to the problem Next bigger number with the same digits (4kyu) of
+Code Wars.
+Description:
+    reate a function that takes a positive integer and returns the next bigger
+    number that can be formed by rearranging its digits.
+"""
+
+
+def get_smallest_bigger_int_with_same_digits(int_: int) -> int:
+    raise NotImplementedError
